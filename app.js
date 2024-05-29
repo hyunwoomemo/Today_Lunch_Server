9b231602a35bfde94c6d8f42371fbd078059cbdf
leehyunwoo <archihw94@gmail.com>
2024-05-29 14:46:31 +0900

    cors 추가

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+app.get("/123", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
