3357adc88036307770c91bcef2654fa8f44dcdca
leehyunwoo <archihw94@gmail.com>
2024-06-18 01:18:49 +0900

    그룹 리스트 불러오기

--- controller/groupController.js
+++ controller/groupController.js
@@ -25,3 +25,19 @@ exports.createGroup = async (req, res) => {
     res.sendStatus(500);
   }
 };
+
+exports.getList = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const [row] = await db.query("select g.group_id, g.name, g.createdAt, g.leader from `groups` g join user_groups ug on ug.group_id = g.group_id where ug.user_id = (?)", [userId]);
+
+    console.log("sdm,fsmdkf", row);
+
+    res.json({ CODE: "GL000", DATA: row });
+    console.log("sdm,fsmdkf");
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+};
--- routes/group.js
+++ routes/group.js
@@ -6,5 +6,6 @@ const groupController = require("../controller/groupController");
 const { authCheck } = require("../middleware/authCheck");
 
 router.post("/createGroup", authCheck, groupController.createGroup);
+router.get("/getGroupList/:userId", authCheck, groupController.getList);
 
 module.exports = router;
